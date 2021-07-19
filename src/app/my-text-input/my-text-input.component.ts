1f6a30c6baf50ccd62979ca19bf92a6f25fcd10a
Lekkyjay <lekkyjayjay@yahoo.com>
2021-07-19 20:22:30 +0200

    Added ControlValueAccessor interface.

--- src/app/my-text-input/my-text-input.component.ts
+++ src/app/my-text-input/my-text-input.component.ts
@@ -1,13 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-my-text-input',
   templateUrl: './my-text-input.component.html',
-  styleUrls: ['./my-text-input.component.scss']
+  styleUrls: ['./my-text-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => MyTextInputComponent),
+      multi: true,
+    },
+  ]
 })
-export class MyTextInputComponent implements OnInit {
+export class MyTextInputComponent implements OnInit, ControlValueAccessor {
+  @Input() label!: string;
+  @Input() type: 'text' | 'password' | 'email' = 'text';
+  @Input() placeholder!: string;
 
   constructor() { }
+
+  writeValue(obj: any): void {
+  }
+
+  registerOnChange(fn: any): void {
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+  }
 
   ngOnInit(): void {
   }
